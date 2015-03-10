dabff766f71b498f2ff685fb32840b2e2f3e40b0
DKerr <drew.kerr86@gmail.com>
2015-03-09 19:52:19 -0500

    Working on solving undefined method error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,15 +3,6 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  #before_action :configure_permitted_parameters, if: :devise_controller?
-  def ensure_sign_up_complete
 
-    return if #action_name == 'finish_signup'
 
-    if current_user && !current_user.email_verified?
-      redirect_to
-  #private
-
-  #def configure_permitted_parameters
-   # devise_parameter_sanitizer.for(:sign_up) << :
 end
